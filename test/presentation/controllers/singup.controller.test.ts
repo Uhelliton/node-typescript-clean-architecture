b80df7de481dc4d26f3ad20f4d5bc584c8b67e09
uhelliton.andrade <uhelliton.andrade@megawhat.energy>
2021-03-13 20:49:46 -0300

    test: ensure SingUpController returns 400 if no name as is provided

---
+++ test/presentation/controllers/singup.controller.test.ts
@@ -0,0 +1,15 @@
+import { SingUpController } from '@src/presentation/controllers/singup.controller'
+
+describe('SingUp Controller', () => {
+    test('Should return 400 if no name is provided', () => {
+        // system under test
+        const sut = new SingUpController()
+        const httpRequest = {
+            email: 'any_email',
+            password: 'any_password',
+            passwordConfirmation: 'any_password_confirm',
+        }
+        const httpResponse = sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+    })
+})
