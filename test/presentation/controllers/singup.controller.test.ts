363c0414d295d7ebc134dc19e43b5683ee4e1a35
Uhelliton <uhelliton@uol.com.br>
2021-03-14 12:12:49 -0300

    refactor: move EmailValidator creation to a factor helper method

--- test/presentation/controllers/singup.controller.test.ts
+++ test/presentation/controllers/singup.controller.test.ts
@@ -10,14 +10,26 @@ interface SutTypes {
     emailValidatorStub: EmailValidator
 }
 
-const makeSut = (): SutTypes => {
+const makeEmailValidator = (): EmailValidator => {
     class EmailValidatorStub implements EmailValidator {
         isValid (email: string): boolean {
             return true
         }
     }
+    return new EmailValidatorStub()
+}
 
-    const emailValidatorStub = new EmailValidatorStub()
+const makeEmailValidatorWithError = (): EmailValidator => {
+    class EmailValidatorStub implements EmailValidator {
+        isValid (email: string): boolean {
+            throw new Error()
+        }
+    }
+    return new EmailValidatorStub()
+}
+
+const makeSut = (): SutTypes => {
+    const emailValidatorStub = makeEmailValidator()
     const sut = new SingUpController(emailValidatorStub)
 
     return { sut, emailValidatorStub }
@@ -131,13 +143,7 @@ describe('SingUp Controller', () => {
     })
 
     test('Should return 500 if EmailValidator throws', () => {
-        class EmailValidatorStub implements EmailValidator {
-            isValid (email: string): boolean {
-                throw new Error()
-            }
-        }
-
-        const emailValidatorStub = new EmailValidatorStub()
+        const emailValidatorStub = makeEmailValidatorWithError()
         const sut = new SingUpController(emailValidatorStub)
 
         const httpRequest = {
