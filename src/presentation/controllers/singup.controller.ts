6cacaeb1d1ce84cba3a62f94f80031c8bbad8a84
uhelliton.andrade <uhelliton.andrade@megawhat.energy>
2021-03-13 22:13:34 -0300

    refactor: add helper badRequest method

--- src/presentation/controllers/singup.controller.ts
+++ src/presentation/controllers/singup.controller.ts
@@ -1,19 +1,14 @@
 import { HttpResponse, HttpRequest } from '@src/presentation/protocols/http'
 import { MissingParamError } from '@src/presentation/erros/missing-param.error'
+import { badRequest } from '@src/presentation/helpers/http-helper'
 
 export class SingUpController {
     handle (httpRequest: HttpRequest): HttpResponse|any {
        if (!httpRequest.body.name) {
-          return {
-              statusCode: 400,
-              body: new MissingParamError('name')
-          }
+          return badRequest(new MissingParamError('name'))
        }
-        if (!httpRequest.body.email) {
-            return {
-                statusCode: 400,
-                body: new MissingParamError('email')
-            }
-        }
+       if (!httpRequest.body.email) {
+            return badRequest(new MissingParamError('email'))
+       }
     }
 }
