d89a9bbc472e32c545bdc460af491d1c62b5747c
Uhelliton <uhelliton@uol.com.br>
2021-03-13 21:03:52 -0300

    test: ensure SingUpController returns 400 if no email as is provided

--- test/presentation/controllers/singup.controller.test.ts
+++ test/presentation/controllers/singup.controller.test.ts
@@ -5,13 +5,31 @@ describe('SingUp Controller', () => {
         // system under test
         const sut = new SingUpController()
         const httpRequest = {
-            email: 'any_email',
-            password: 'any_password',
-            passwordConfirmation: 'any_password_confirm',
+            body: {
+                email: 'any_email',
+                password: 'any_password',
+                passwordConfirmation: 'any_password_confirm',
+            }
         }
         const httpResponse = sut.handle(httpRequest)
 
         expect(httpResponse.statusCode).toBe(400)
         expect(httpResponse.body).toEqual(new Error('Mission param: name'))
+    })
+
+    test('Should return 400 if no email is provided', () => {
+        // system under test
+        const sut = new SingUpController()
+        const httpRequest = {
+           body: {
+               name: 'any_name',
+               password: 'any_password',
+               passwordConfirmation: 'any_password_confirm',
+           }
+        }
+        const httpResponse = sut.handle(httpRequest)
+
+        expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(new Error('Mission param: email'))
     })
 })
