fc56c3d149232089556728b758df69537113abad
Uhelliton <uhelliton@uol.com.br>
2021-03-13 22:22:00 -0300

    env: ensure SingUpController returns 400 if no passwordConfirmation as is provided

--- test/presentation/controllers/singup.controller.test.ts
+++ test/presentation/controllers/singup.controller.test.ts
@@ -49,4 +49,20 @@ describe('SingUp Controller', () => {
         expect(httpResponse.statusCode).toBe(400)
         expect(httpResponse.body).toEqual(new MissingParamError('password'))
     })
+
+    test('Should return 400 if no passwordConfirmation is provided', () => {
+        // system under test
+        const sut = new SingUpController()
+        const httpRequest = {
+            body: {
+                name: 'any_name',
+                email: 'any_email',
+                password: 'any_password',
+            }
+        }
+        const httpResponse = sut.handle(httpRequest)
+
+        expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(new MissingParamError('passwordConfirmation'))
+    })
 })
