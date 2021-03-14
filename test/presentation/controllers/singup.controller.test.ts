d17fad4972fefc24c39a611c11144d2a3507a553
uhelliton.andrade <uhelliton.andrade@megawhat.energy>
2021-03-14 13:19:13 -0300

    refactor: change mocking strategy

--- test/presentation/controllers/singup.controller.test.ts
+++ test/presentation/controllers/singup.controller.test.ts
@@ -19,14 +19,14 @@ const makeEmailValidator = (): EmailValidator => {
     return new EmailValidatorStub()
 }
 
-const makeEmailValidatorWithError = (): EmailValidator => {
-    class EmailValidatorStub implements EmailValidator {
-        isValid (email: string): boolean {
-            throw new Error()
-        }
-    }
-    return new EmailValidatorStub()
-}
+// const makeEmailValidatorWithError = (): EmailValidator => {
+//     class EmailValidatorStub implements EmailValidator {
+//         isValid (email: string): boolean {
+//             throw new Error()
+//         }
+//     }
+//     return new EmailValidatorStub()
+// }
 
 const makeSut = (): SutTypes => {
     const emailValidatorStub = makeEmailValidator()
@@ -143,8 +143,10 @@ describe('SingUp Controller', () => {
     })
 
     test('Should return 500 if EmailValidator throws', () => {
-        const emailValidatorStub = makeEmailValidatorWithError()
-        const sut = new SingUpController(emailValidatorStub)
+        const { sut, emailValidatorStub } = makeSut()
+        jest.spyOn(emailValidatorStub, 'isValid').mockImplementationOnce(() => {
+            throw new Error()
+        })
 
         const httpRequest = {
             body: {
