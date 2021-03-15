dee04df1c6555805f91f9f51be0b3c268e17e301
Uhelliton <uhelliton@uol.com.br>
2021-03-14 21:21:37 -0300

    test: ensure SingUpController returns 500 if AddAccount throw

--- test/presentation/controllers/singup.controller.test.ts
+++ test/presentation/controllers/singup.controller.test.ts
@@ -219,4 +219,24 @@ describe('SingUp Controller', () => {
         })
     })
 
+    test('Should return 500 if AddAccount throws', () => {
+        const { sut, emailValidatorStub } = makeSut()
+        jest.spyOn(emailValidatorStub, 'isValid').mockImplementationOnce(() => {
+            throw new Error()
+        })
+
+        const httpRequest = {
+            body: {
+                name: 'any_name',
+                email: 'any_email@email.com',
+                password: 'any_password',
+                passwordConfirmation: 'any_password',
+            }
+        }
+        const httpResponse = sut.handle(httpRequest)
+
+        expect(httpResponse.statusCode).toBe(500)
+        expect(httpResponse.body).toEqual(new ServerError())
+    })
+
 })
